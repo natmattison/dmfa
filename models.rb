a2f66bf36d2f7b6de9484958ec09cd41a857073f
natmat <natmattison@gmail.com>
2015-07-17 10:40:25 -0700

    Adding next and previous arrows to detail page

--- models.rb
+++ models.rb
@@ -25,4 +25,12 @@ end
 class Painting < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
+  def next
+    Painting.where('id > ?', self.id).pluck(:id).min
+  end
+  
+  def previous
+    Painting.where('id < ?', self.id).pluck(:id).max
+  end
+
 end
