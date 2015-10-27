fa0ff6012fb8b7a80c9dee83857f6947ea3999d0
natmat <natmattison@gmail.com>
2015-10-27 16:46:46 -0700

    Adding upload script to sync database with CSV file

---
+++ scripts/upload.rb
@@ -0,0 +1,34 @@
+require 'pg'
+require 'csv'
+
+csv = CSV.read("paintings.csv", :headers => true)
+
+csv_hash = csv.map(&:to_hash)
+
+conn = PG.connect(host: ENV['DB_HOST'], port: ENV['DB_PORT'], user: ENV['DB_USER'], password: ENV['DB_PASSWORD'], dbname: ENV['DB_NAME'])
+
+conn.exec('DELETE FROM paintings')
+
+csv_hash.each do |row|
+  begin
+    id = row['id']
+    name = row['name']
+    description = row['description'] || ""
+    height = row['height']
+    width = row['width']
+    sold = row['price'] != nil
+    category = row['category']
+    medium = row['medium'].downcase
+    thumbnail_url = row['thumbnail_url'].gsub('www.dropbox.com', 'dl.dropboxusercontent.com')
+    fullsize_url = row['fullsize_url'].gsub('www.dropbox.com', 'dl.dropboxusercontent.com')
+    price = row['price'] || nil
+    showcase = row['showcase?'] == 'y'
+
+    puts 'INSERT INTO paintings VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)' % [id, name, description, height, width, sold, category, medium, thumbnail_url, fullsize_url, price, showcase]
+
+    conn.exec('INSERT INTO paintings VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)', [id, name, description, height, width, sold, category, medium, thumbnail_url, fullsize_url, price, showcase])
+  rescue StandardError => e
+    puts e
+    next
+  end
+end
